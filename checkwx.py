da2848aea3bcc5362ef06ee5d36a779aad5f4bf7
Hallie Leonhardy <hleonhar@mail.ccsf.edu>
2017-11-07 17:06:22 -0800

    program can now spit out the forecast times (Z) and the types/heights of clouds

--- checkwx.py
+++ checkwx.py
@@ -4,9 +4,40 @@ import requests
 
 API_KEY = os.environ['CHECKWXAPIKEY']
 
-url = 'https://api.checkwx.com/metar/lat/51.4706/lon/-1.461941/decoded'
-headers = {API_KEY: '{key}'}
+def print_response(icao):
 
-response = requests.get(url, headers=headers)
+    url = 'https://api.checkwx.com/taf/{}/decoded'.format(icao)
+    headers = { 'X-API-Key': API_KEY }
 
-print response
+    response = requests.get(url, headers=headers)
+    json_response = response.json()
+
+    data = json_response['data']
+
+    #I guess data was a list with only one item (dictionary)
+    #So now it is a dictionary:
+    data = data[0]
+
+    #Getting a list of all forecasts
+    #Each index in the list is the forecast(dictionary)
+    forecasts = data['forecast']
+
+    for forecast_dict in forecasts:
+
+        timestamp = forecast_dict['timestamp']
+        forecast_to = timestamp['forecast_to']
+        forecast_from = timestamp['forecast_from']
+
+        clouds_list = forecast_dict['clouds']
+
+        print "Forecast TO: {} \n Forecast FROM: {}".format(forecast_to, forecast_from)
+        
+        for item in clouds_list:
+            cloud_type = item['code']
+            cloud_height = item['base_feet_agl']
+
+            print "{} @ {} ft.".format(cloud_type, cloud_height)
+        print
+
+
+print_response('ksea')
