e53c517fc13bc6fb4ef8cb7156f09f061e4dd53c
Hallie Leonhardy <hleonhar@mail.ccsf.edu>
2017-11-27 17:25:22 -0800

    Made photo and favorites add on my page into modal windows

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -26,10 +26,16 @@ function initAutocomplete() {
       /** @type {!HTMLInputElement} */(document.getElementById('nav-autocomplete')),
       {types: ['geocode']});
 
+  photoAutocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('photo-autocomplete')),
+      {types: ['geocode']});
+
   // When the user selects an address from the dropdown, populate the address
   // fields in the form.
   autocomplete.addListener('place_changed', fillInAddress);
   navAutocomplete.addListener('place_changed', fillInAddressNav)
+  photoAutocomplete.addListener('place_changed', fillInAddressNav)
+
 }
 
 
@@ -111,5 +117,41 @@ function geolocateNav() {
   }
 }
 
+function fillInAddressPhoto() {
+  // Get the place details from the autocomplete object.
+  var place = photoAutocomplete.getPlace();
 
+  for (var component in componentForm) {
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
+  }
+
+  // Get each component of the address from the place details
+  // and fill the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocatePhoto() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
 
